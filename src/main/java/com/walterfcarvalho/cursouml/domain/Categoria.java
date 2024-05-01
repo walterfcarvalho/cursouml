149354e50b779bf878772f7772e176d88f95c38e
valter.carvalho <valter.carvalho@totvspartners.com.br>
2024-05-01 15:45:46 +0100

    primeira classe dominio - categoria

---
+++ src/main/java/com/walterfcarvalho/cursouml/domain/Categoria.java
@@ -0,0 +1,67 @@
+package com.walterfcarvalho.cursouml.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+ 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        return prime * result + ((id == null) ? 0 : id.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categoria other = (Categoria) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
--- src/main/java/com/walterfcarvalho/cursouml/resources/CategoriaResource.java
+++ src/main/java/com/walterfcarvalho/cursouml/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.walterfcarvalho.cursouml.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.walterfcarvalho.cursouml.domain.Categoria;;
 
-@RestController
+@RestController 
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "rest está ok ! ";
+    public List<Categoria> listar(){
+        Categoria c1 = new Categoria(1, "Categoria 02");
+        Categoria c2 = new Categoria(1, "Categoria 01");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(c1);
+        lista.add(c2);
+
+        return lista;
     }
 }
