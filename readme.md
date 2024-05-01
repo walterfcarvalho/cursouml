add07c4fa2c159bffeb214adecd6e688234dc5c1
valter.carvalho <valter.carvalho@totvspartners.com.br>
2024-05-01 17:00:13 +0100

    configuracao do JPA

---
+++ readme.md
@@ -0,0 +1,3 @@
+https://code.visualstudio.com/docs/java/java-spring-boot
+
+https://dev.to/kenji_goh/set-up-spring-boot-java-application-in-vs-code-3aln
--- src/main/java/com/walterfcarvalho/cursouml/domain/Categoria.java
+++ src/main/java/com/walterfcarvalho/cursouml/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.walterfcarvalho.cursouml.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
  
