40f1df1dc821ec02eb89eb8281db64329098cb24
valter.carvalho <valter.carvalho@totvspartners.com.br>
2024-05-01 14:52:20 +0100

    first commit

---
+++ src/main/java/com/walterfcarvalho/cursouml/CursoumlApplication.java
@@ -0,0 +1,13 @@
+package com.walterfcarvalho.cursouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoumlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walterfcarvalho/cursouml/CursoumlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walterfcarvalho.cursouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
